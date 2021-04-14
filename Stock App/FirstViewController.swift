0fa86baacd11101d3f832023f4c31939c603c0d5
Ammar Rusydi <ammar@massiveinfinity.com>
2021-04-14 23:38:55 +0800

    Initial Commit

---
+++ Stock App/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Stock App
+//
+//  Created by Massive Infinity on 14/4/21.
+//  Copyright © 2021 ammar. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Stock App/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Stock App
+//
+//  Created by Massive Infinity on 14/4/21.
+//  Copyright © 2021 ammar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
